3764b6011599bc61169d7c9bdac43a05973226a5
Andrew Innocent <innocent.andrew@gmail.com>
2020-02-23 22:18:38 -0500

    Adds basic content and imports css

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@
  *      (tip: React normalizes <textarea /> to be more like <input />, 
  *      so it can be used as a self-closing element and uses the `value` property
  *      to set its contents)
- * 3. <h4> ti display the amount of time remaining
+ * 3. <h4> to display the amount of time remaining
  * 4. <button> to start the game
  * 5. Another <h1> to display the word count
  */
@@ -15,7 +15,11 @@ import React from "react"
 function App() {    
     return (
         <div>
-           <h1>Hello!</h1>
+          <h1>Speed Typing Game!</h1>
+          <textarea value=""/>
+          <h4>Time remaining: [seconds]</h4>
+          <button>Start</button>
+          <h1>Word count: [x]</h1>
         </div>
     )
 }
