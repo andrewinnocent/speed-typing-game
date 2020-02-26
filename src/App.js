c7951365de1ea5a03f0f72bbfe18448205de04c3
Andrew Innocent <innocent.andrew@gmail.com>
2020-02-25 21:02:23 -0500

    Clicking Start button again begins another game

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-
 /**
  * Challenge:
  * 
- * Make it so clicking the Start button starts the timer instead of it starting on refresh
- * (Hint: use a new state variable to indicate if the game should be running or not)
+ * When the timer reaches 0, count the number of words the user typed in 
+ * and display it in the "Word count" section
+ * 
+ * After the game ends, make it so the user can click the Start button again
+ * to play a second time
  */
 
 import React, {useState, useEffect} from "react"
@@ -19,34 +21,39 @@ function App() {
 		}
 
 		useEffect(() => {
-			console.log(startedTimer)
 			let timeoutId
+
 			if (timeRemaining > 0 && startedTimer) {
 				timeoutId = setTimeout(() => {
 				setTimeRemaining(prevTimeRemaining => prevTimeRemaining - 1)
-			}, 1000)} else if(!timeRemaining) {
+			}, 1000)} else if (!timeRemaining) {
+				console.log('time is up!')
 				setStartedTimer(false)
 			}
-			
+
 			return () => clearTimeout(timeoutId)
 			
 		}, [startedTimer, timeRemaining]) 
 
 
-		// function getWordsCount(str) {
-		// 	return str.split(" ")
-    //         .filter(n => n !== "")
-    //         .length;
-		// }
+		function getWordsCount(str) {
+			return str.split(" ")
+            .filter(n => n !== "")
+            .length;
+		}
 
-
+		function beginTimer() {
+			setStartedTimer(true)
+			if (!timeRemaining) setTimeRemaining(2)
+		}
+		
     return (
         <div>
           <h1>Speed Typing Game!</h1>
           <textarea value={words} onChange={updateWords}/>
           <h4>Time remaining: {timeRemaining}</h4>
-          <button onClick={() => setStartedTimer(true)}>Start</button>
-          <h1>Word count: [x]</h1>
+          <button onClick={() => beginTimer()}>Start</button>
+          <h1>Word count: {!timeRemaining ? getWordsCount(words) : 0}</h1>
         </div>
     )
 }
