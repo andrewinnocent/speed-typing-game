794a709a3ca0f2664e9dc09f575625adca7f7554
Andrew Innocent <innocent.andrew@gmail.com>
2020-02-25 20:28:30 -0500

    Begins the timer on clicking the Start button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
+
 /**
  * Challenge:
  * 
- * 1. Create state to hold the current value of the countdown timer.
- *    Display this time in the "Time Remaining" header
- * 2. Set up an effect that runs every time the `timeRemaining` changes
- *    The effect should wait 1 second, then decrement the `timeRemaining` by 1
- * 
- *    Hint: use `setTimeout` instead of `setInterval`. This will help you avoid
- *    a lot of extra work.
- * 
- *    Warning: there will be a bug in this, but we'll tackle that next (AI note: The bug is an infinite loop of render/useEffect.)
- * 3. Make it so the effect won't run if the time is already at 0
+ * Make it so clicking the Start button starts the timer instead of it starting on refresh
+ * (Hint: use a new state variable to indicate if the game should be running or not)
  */
 
 import React, {useState, useEffect} from "react"
 
 function App() {   
 		const [words, setWords] = useState("")
-		const [timeRemaining, setTimeRemaining] = useState(5)
+		const [timeRemaining, setTimeRemaining] = useState(2)
+		const [startedTimer, setStartedTimer] = useState(false)
 
 		function updateWords(e) {
 			const {value} = e.target
@@ -25,29 +19,33 @@ function App() {
 		}
 
 		useEffect(() => {
+			console.log(startedTimer)
 			let timeoutId
-			if (timeRemaining > 0) {
+			if (timeRemaining > 0 && startedTimer) {
 				timeoutId = setTimeout(() => {
 				setTimeRemaining(prevTimeRemaining => prevTimeRemaining - 1)
-			}, 1000)}
+			}, 1000)} else if(!timeRemaining) {
+				setStartedTimer(false)
+			}
 			
 			return () => clearTimeout(timeoutId)
 			
-		}, [timeRemaining]) 
+		}, [startedTimer, timeRemaining]) 
 
 
-		function getWordsCount(str) {
-			return str.split(" ")
-            .filter(n => n !== "")
-            .length;
-		}
-		
+		// function getWordsCount(str) {
+		// 	return str.split(" ")
+    //         .filter(n => n !== "")
+    //         .length;
+		// }
+
+
     return (
         <div>
           <h1>Speed Typing Game!</h1>
           <textarea value={words} onChange={updateWords}/>
           <h4>Time remaining: {timeRemaining}</h4>
-          <button onClick={() => console.log(getWordsCount(words))}>Start</button>
+          <button onClick={() => setStartedTimer(true)}>Start</button>
           <h1>Word count: [x]</h1>
         </div>
     )
